2b5784484b14309855971d6044033138c12c7a4b
Lucian Duta <duta39@gmail.com>
2022-05-08 03:31:03 +0100

    App routes and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar";
+import CalcForm from "./components/CalcForm/CalcForm";
+import CalculatorScreen from "./screens/CalculatorScreen";
 
 function App() {
-  return <div className="App">hellp</div>;
+  return (
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CalculatorScreen />} />
+        <Route path="/about" element={<CalcForm />} />
+        <Route path="/contact" element={<CalcForm />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
