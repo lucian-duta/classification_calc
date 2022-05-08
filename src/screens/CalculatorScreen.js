7d9b19675f22c23b2438c2f75474e637e7da50e2
Lucian Duta <duta39@gmail.com>
2022-05-08 03:31:35 +0100

    Added calc screen

---
+++ src/screens/CalculatorScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CalcForm from "../components/CalcForm/CalcForm";
+
+const CalculatorScreen = () => {
+  return (
+    <>
+      <CalcForm />
+    </>
+  );
+};
+
+export default CalculatorScreen;
